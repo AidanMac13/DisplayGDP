a3ad0424cf9ea44658995aed20164067b519c944
Aidan MacNevin <ahmacnevin@wpi.edu>
2023-03-10 13:59:56 -0500

    Sample graphs showing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import urllib3
 import seaborn as sns
+import matplotlib.pyplot as plt
 import json
 
 http = urllib3.PoolManager()
@@ -7,15 +8,18 @@ sns.set_theme()
 
 r = http.request('GET', 'https://api.stlouisfed.org/fred/series?series_id=GDP&api_key=b4a7d4f07a04381d20dc6002f4d15178')
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
+tips = sns.load_dataset("tips")
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print(r.data)
-    print_hi('PyCharm')
+    # print(r.data)
+    # print(tips)
+    sns.relplot(
+        data=tips,
+        x="total_bill", y="tip", col="time",
+        hue="smoker", style="smoker", size="size",
+    )
+    plt.show()
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
