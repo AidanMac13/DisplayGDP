595af666a33539b1361d4b3d5d0b05302c3383e1
Aidan MacNevin <ahmacnevin@wpi.edu>
2023-03-10 14:21:24 -0500

    Downloaded GDP data from fred and plots basic in seaborn

--- main.py
+++ main.py
@@ -1,23 +1,27 @@
 import urllib3
 import seaborn as sns
+import fredapi as fa
+import pandas as pd
+
 import matplotlib.pyplot as plt
 import json
 
 http = urllib3.PoolManager()
 sns.set_theme()
 
-r = http.request('GET', 'https://api.stlouisfed.org/fred/series?series_id=GDP&api_key=b4a7d4f07a04381d20dc6002f4d15178')
+fred = fa.Fred("b4a7d4f07a04381d20dc6002f4d15178")
 
-tips = sns.load_dataset("tips")
+r = http.request('GET', 'https://api.stlouisfed.org/fred/series?series_id=GDP&api_key'
+                        '=b4a7d4f07a04381d20dc6002f4d15178&file_type=json')
+
+gdp = fred.get_series('GDP')
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    # print(r.data)
-    # print(tips)
+    print(r.data)
+    print(gdp)
     sns.relplot(
-        data=tips,
-        x="total_bill", y="tip", col="time",
-        hue="smoker", style="smoker", size="size",
+        data=gdp
     )
     plt.show()
 
