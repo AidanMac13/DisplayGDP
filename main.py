2d2892f639f63a79db01bea4f8e8c045b30bb0e1
Aidan MacNevin <ahmacnevin@wpi.edu>
2023-03-27 17:28:24 -0400

    Security Fix

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import config
+
 import urllib3
 import seaborn as sns
 import fredapi as fa
@@ -7,7 +9,7 @@ import matplotlib.pyplot as plt
 http = urllib3.PoolManager()
 sns.set_theme(style="darkgrid")
 
-fred = fa.Fred("b4a7d4f07a04381d20dc6002f4d15178")
+fred = fa.Fred(config.fred_key)
 
 
 gdp = fred.get_series('GDP')
